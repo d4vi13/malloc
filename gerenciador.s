3f76d57a6dd85c24c1fa319bda89190629992917
dcr23 <dcr23@inf.ufpr.br>
2024-10-25 19:21:41 -0300

    lastest version intel syntax

--- gerenciador.s
+++ gerenciador.s
@@ -1,16 +1,24 @@
 .intel_syntax noprefix
 .section .data
-    msg: .string "Topoo inicial da heap: %lld\n"
-.section .bss
-    .lcomm topoInicialHeap 8 
+    msg: .string "Topo inicial da heap: %ld\n\0" 
+    num: .quad 5
+#.section .bss
+#   .lcomm topoInicialHeap 8 
 
 .section .text
 .globl main, iniciaAlocador, finalizaAlocador, alocaMem, liberaMem
 
-main:#iniciaAlocador:
-    mov rsi, topoInicialHeap 
-    mov rdi, qword ptr [msg]
+main:
+    push rbp
+    mov rbp, rsp
+
+    mov rdi, msg
+    mov rax, [num]
+    mov rsi, rax
     call printf 
+
+    pop rbp
+    ret
 
 finalizaAlocador:
 

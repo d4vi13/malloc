9aa96c874d0cfcb3e6e28aa6a52c99ea80e3a0c8
davi <dcr23@inf.ufpr.br>
2024-11-11 11:54:01 -0300

    IniciaAlocador implementado

--- gerenciador.s
+++ gerenciador.s
@@ -1,24 +1,25 @@
-.intel_syntax noprefix
 .section .data
     msg: .string "Topo inicial da heap: %ld\n\0" 
+    printf_alloc: .string "Chamada do printf para alocação de seu buffer operacional\n"
     num: .quad 5
-#.section .bss
-#   .lcomm topoInicialHeap 8 
+
+.section .bss:
+    .lcomm topoInicialHeap, 8
 
 .section .text
-.globl main, iniciaAlocador, finalizaAlocador, alocaMem, liberaMem
+.globl iniciaAlocador, finalizaAlocador, alocaMem, liberaMem
 
-main:
-    push rbp
-    mov rbp, rsp
-
-    mov rdi, msg
-    mov rax, [num]
-    mov rsi, rax
+iniciaAlocador:
+    pushq %rbp
+    movq %rsp, %rbp
+    movq $printf_alloc, %rdi
     call printf 
-
-    pop rbp
-    ret
+    movq $0, %rdi
+    movq $12, %rax
+    syscall
+    movq %rax, topoInicialHeap
+    popq %rbp
+    ret     
 
 finalizaAlocador:
 

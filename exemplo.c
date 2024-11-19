5c75516daf4a913604d9f8f1701d8f32b37acb87
dcr23 <dcr23@inf.ufpr.br>
2024-11-19 09:15:40 -0300

    Stable version(aloca e libera should be working)

--- exemplo.c
+++ exemplo.c
@@ -10,23 +10,34 @@ typedef struct metadata metadata, *pmetadata;
     
 
 int main () {
-  void *a, *b, *c;
-  pmetadata ma, mb, mc;
+  void *a, *b, *c, *d;
+  pmetadata ma, mb, mc, md;
   iniciaAlocador();               // Impressão esperada
 
 //  imprimeMapa();                  // <vazio>
 //
     a = (void *) alocaMem(10);
     ma = a - 16;
-    printf("%ld %ld\n",ma->used, ma->size);
+    printf("%p %ld %ld\n",ma,ma->used, ma->size);
 //  imprimeMapa();                  // ################**********
     b = (void *) alocaMem(4);
     mb = b - 16;
-    printf("%ld %ld\n",mb->used, mb->size);
+    printf("%p %ld %ld\n",mb, mb->used, mb->size);
     c = (void *) alocaMem(8000);
     mc = c - 16;
-    printf("%ld %ld\n",mc->used, mc->size);
- 
+    printf("%p %ld %ld\n",mc, mc->used, mc->size);
+    d = (void *) alocaMem(3000);
+    md = d - 16;
+    liberaMem(d);
+    printf("%p %ld %ld\n",md, md->used, md->size);
+    d = (void *) alocaMem(500);
+    md = d - 16;
+    liberaMem(d);
+    printf("%p %ld %ld\n",md, md->used, md->size);
+
+    printf("%ld\n", mb-ma); 
+    printf("%ld\n", md-mb); 
+    printf("%ld\n", mc-mb); 
 //  imprimeMapa();                  // ################**********##############****
 //  liberaMem(a);
 //  imprimeMapa();                  // ################----------##############****
--- gerenciador.s
+++ gerenciador.s
@@ -180,5 +180,19 @@ exit:
     ret    
 
 liberaMem:
+    pushq %rbp
+    movq %rsp, %rbp
+   
+    movq $0, -16(%rdi)
+ 
+    popq %rbp
+    ret 
+
+
+
+
+
+
+
 
 

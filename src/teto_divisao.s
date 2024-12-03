cc71e543f6e046356a0eb2b7f5892b6966023196
davi <dcr23@inf.ufpr.br>
2024-12-03 10:29:51 -0300

    adiciona explicacao no teto_divisao]

--- src/teto_divisao.s
+++ src/teto_divisao.s
@@ -4,12 +4,17 @@
 teto_divisao:
     pushq %rbp
     movq %rsp, %rbp                                                                                                                                                       
-
+    // move valor para rax e extende sinal para octal
     movq %rdi, %rax
     cqto
-    idivq %rsi
-    testq %rdx, %rdx
-    jz sem_resto
+    
+    // divide rdi/rsi
+    idivq %rsi 
+   
+    // rdx contem resto, ve se tem resto  
+    movq $0, %rdi
+    cmpq %rdx, %rdi
+    je sem_resto
     addq $1, %rax
 
 sem_resto:
